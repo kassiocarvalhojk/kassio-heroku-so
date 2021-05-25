6a19c0798e7b8ad6c0528df0081f48f94809d014
kassiocarvalhojk <82849257+kassiocarvalhojk@users.noreply.github.com>
2021-05-25 13:30:31 -0300

    Update web.py

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Kássio Pereira de Carvalho"
+    return "Kássio Pereira de Carvalho RA 1904698"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
